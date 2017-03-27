691bf6d128cd8c7d67e3f5e049db9dcbfe47fb7e
cusengan <lordvinh1@gmail.com>
2017-03-27 17:49:14 -0500

    Updated scrum sheet and added a use case diagram

--- Code/Controller.cpp
+++ Code/Controller.cpp
@@ -1,16 +1,5 @@
 #include "Controller.h"
 
-// std::string name;
-// 	int model_num;
-
-// 	std::cout << "Name?" << std::endl;
-// 	getline(std::cin, name);
-
-// 	std::cout << "Model number?" << std::endl;
-// 	std::cin >> model_num;
-// 	std::cin.ignore();
-//these were in the create_robot_part functions in shop
-
 void Controller::execute_cmd(int cmd){
 	
 	//create a part
